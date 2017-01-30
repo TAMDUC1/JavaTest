b320b03d34f89735a4b27297799c71da22722316
TamDuc <tamduc@stud.ntnu.no>
2017-01-30 11:40:48 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print('f');
+        System.out.print('k');
+
+
     }
 }
